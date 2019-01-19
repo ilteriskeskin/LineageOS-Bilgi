6c3983de2473107ab09544b7d2f131073b5c668e
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2019-01-19 15:57:08 +0300

    Yeni templateler eklendi, web üzerinde LineageOS destekelyen modeller görüntülenmeye başlandı

--- app.py
+++ app.py
@@ -13,6 +13,31 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/list')
+def list():
+    os.system("ls lineage_wiki/_data/devices/ > lineage_wiki/_data/devices/output.txt")
+    file_names = []
+
+    f = open("lineage_wiki/_data/devices/output.txt")
+
+    for i in f:
+        a = f.readline()
+        file_names.append(a)
+
+    for i in range(len(file_names)):
+
+        file_path = "/home/ilteriskeskin/Belgeler/Python/lineage_wiki/_data/devices/" + file_names[i].rstrip()
+
+        with open(file_path, "r") as stream:
+            try:
+                file = yaml.load(stream)
+                #print(i + 1,file["vendor"], file["name"])
+                file_names.append(file)
+            except:
+                print()
+    
+    return render_template('list.html', file_names = file_names)
+
 @app.route('/benchmark')
 def benchmark():
     os.system("ls lineage_wiki/_data/devices/ > lineage_wiki/_data/devices/output.txt")
@@ -32,12 +57,11 @@ def benchmark():
             try:
                 file = yaml.load(stream)
                 #print(i + 1,file["vendor"], file["name"])
-
+                file_names.append(file)
             except:
                 print()
-
-        return render_template('benchmark.html', file = file)
-    return render_template('benchmark.html')
+    
+    return render_template('benchmark.html', file_names = file_names)
     
 if __name__ == '__main__':
     app.run(debug=True)
